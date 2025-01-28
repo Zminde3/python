a676df46a434c983f602b1e272ef37452b0bf3a6
Zminde3 <zminde3@gmail.com>
2025-01-28 19:14:17 +0200

    Merge remote-tracking branch 'origin/main'

--- 0128_2.py
+++ 0128_2.py
@@ -3,3 +3,11 @@ import datetime as dt
 print(f"Atsitiktinis skaičius: {randint(1, 10)}")
 print(f"Atsitiktinis vaisius: {choice(['obuolys', 'bananas', 'kriaušė', 'vyšnia'])}")
 print(f"Dabartinė data ir laikas: {dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+
+from datetime import datetime
+print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+from datetime import datetime as d
+print(d.now().strftime("%F %T"))
+
+
---
+++ 0129_3.py
@@ -0,0 +1,6 @@
+from math import sqrt as sq
+print(sq(625))
+
+from math import *
+print(sin(90))
+
